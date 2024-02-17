17bf72de6b5da815d2f2ebe3200e0c54efe72802
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-17 14:34:02 +0000

    add icons & update class names

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,18 @@
 import styles from "./Header.module.css";
+import { IoSearch } from "react-icons/io5";
+import { RiShoppingBagLine } from "react-icons/ri";
 
 function Header() {
   return (
     <header>
       <div className={styles.searchBox}>
-        <label htmlFor="search"></label>
+        <IoSearch className={`${styles.searchIcon} ${styles.icon}`} />
       </div>
-      <div className={styles.logoBox}>
+      <div className={styles.headingBox}>
         <h1>LOREMIPSUM</h1>
       </div>
       <div className={styles.basketBox}>
-        <p>Basket</p>
+        <RiShoppingBagLine className={`${styles.basketIcon} ${styles.icon}`} />
       </div>
     </header>
   );
