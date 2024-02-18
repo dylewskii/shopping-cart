d9577d7e26daddb88c23b967d30e7970266225a2
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-18 18:10:23 +0000

    add ShopBanner styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
+import ShopBanner from "./components/shop-banner/ShopBanner";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <ShopBanner></ShopBanner>
     </>
   );
 }
