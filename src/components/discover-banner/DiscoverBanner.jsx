00dcf02e4fac7097778b5f2ad84fbb5c71b039d5
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-18 18:52:48 +0000

    migrate DiscoverBanner code to HeroTwo component

--- src/components/discover-banner/DiscoverBanner.jsx
+++ src/components/discover-banner/DiscoverBanner.jsx
@@ -1,23 +1,7 @@
-import styles from "./DiscoverBanner.module.css";
+// import styles from "./DiscoverBanner.module.css";
 
 function DiscoverBanner() {
-  return (
-    <section className={styles.discoverBanner}>
-      <div className={styles.discoverBannerHeader}>
-        <h2>Make a Statement With These Pieces</h2>
-        <p>
-          This curation of pieces literally makes a statement, whether it be a
-          bold fashion sense or eye-catching detail. Elevate your outfit with a
-          new color, pattern or silhouette.
-        </p>
-      </div>
-      <div className={styles.discoverBannerImg}>
-        <div className={styles.discoverBannerImgWrapper}>
-          <img src="" alt="Discover the Latest Fashion Pieces" />
-        </div>
-      </div>
-    </section>
-  );
+  return;
 }
 
 export default DiscoverBanner;
