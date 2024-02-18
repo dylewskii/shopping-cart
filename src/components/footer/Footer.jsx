f1f6d0c957ad3ffe48e1ef8358c6f36a4c5fdafc
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-18 19:39:55 +0000

    add Footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,48 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+  let currYear = new Date();
+  currYear = currYear.getFullYear();
+
+  return (
+    <footer>
+      <div className={styles.glossaryBox}>
+        <ul>
+          <li>
+            <a>About Us</a>
+          </li>
+          <li>
+            <a>Shop</a>
+          </li>
+          <li>
+            <a>Discover</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+          <li>
+            <a>Privacy</a>
+          </li>
+          <li>
+            <a>Terms</a>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.disclaimerBox}>
+        <h6>DISCLAIMER</h6>
+        <p>
+          Please be aware that our efforts to maintain accessibility and
+          usability are ongoing. While we strive to make the Website as
+          accessible as possible some issues can be encountered by different
+          assistive technology as the range of assistive technology is wide and
+          varied.
+        </p>
+      </div>
+      <div className={styles.copyrightBox}>
+        <p>&copy; Copyright {currYear}</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
