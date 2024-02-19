18273c79257703295325f060e32ec8e816ad9241
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-19 10:48:03 +0000

    add icon & remove unused wrapper

--- src/components/footer/SignUp.jsx
+++ src/components/footer/SignUp.jsx
@@ -1,17 +1,19 @@
 import styles from "./SignUp.module.css";
+import { FaArrowRight } from "react-icons/fa6";
 
 function SignUp() {
   return (
     <div className={styles.signUpBox}>
-      <div className={styles.signUpWrapper}>
-        <label>Sign Up & Don&#39;t Miss Our Latest Drops</label>
-        <input className={styles.signUpInput} />
-        <p>
-          To learn how we process your data, visit our{" "}
-          <a className={styles.privacyLink}>Privacy Notice</a>. You can
-          unsubscribe at any time.
-        </p>
-      </div>
+      <label>Sign Up & Don&#39;t Miss Our Latest Drops</label>
+      <span className={styles.inputSpan}>
+        <input className={styles.signUpInput} placeholder="Enter Your Email" />
+        <FaArrowRight className={styles.icon} />
+      </span>
+      <p>
+        To learn how we process your data, visit our{" "}
+        <a className={styles.privacyLink}>Privacy Notice</a>. You can
+        unsubscribe at any time.
+      </p>
     </div>
   );
 }
