af283f5bb42a05f88756f2a7c688e1d7c13d320c
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-19 12:26:56 +0000

    add wrappers to footer content

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,44 +7,56 @@ function Footer() {
 
   return (
     <footer>
-      <SignUp />
-      <div className={styles.appBox}>
-        <h5>Continue Shopping With Our Brand New App</h5>
+      <div className={styles.footerTop}>
+        <SignUp />
+        <div className={styles.appBox}>
+          <h5>Continue Shopping With Our Brand New App</h5>
+        </div>
+        <div className={styles.glossaryBox}>
+          <ul>
+            <li>
+              <a>About Us</a>
+            </li>
+            <li>
+              <a>Shop</a>
+            </li>
+            <li>
+              <a>Discover</a>
+            </li>
+            <li>
+              <a>Contact</a>
+            </li>
+            <li>
+              <a>Privacy</a>
+            </li>
+            <li>
+              <a>Terms</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className={styles.glossaryBox}>
-        <ul>
-          <li>
-            <a>About Us</a>
-          </li>
-          <li>
-            <a>Shop</a>
-          </li>
-          <li>
-            <a>Discover</a>
-          </li>
-          <li>
-            <a>Contact</a>
-          </li>
-          <li>
-            <a>Privacy</a>
-          </li>
-          <li>
-            <a>Terms</a>
-          </li>
-        </ul>
-      </div>
-      <div className={styles.disclaimerBox}>
-        <h6>DISCLAIMER</h6>
-        <p>
-          Please be aware that our efforts to maintain accessibility and
-          usability are ongoing. While we strive to make the Website as
-          accessible as possible some issues can be encountered by different
-          assistive technology as the range of assistive technology is wide and
-          varied.
-        </p>
-      </div>
-      <div className={styles.copyrightBox}>
-        <p>&copy; Copyright {currYear}</p>
+      <div className={styles.footerBottom}>
+        <div className={styles.disclaimerBox}>
+          <h6>DISCLAIMER</h6>
+          <p>
+            Please be aware that our efforts to maintain accessibility and
+            usability are ongoing. While we strive to make the Website as
+            accessible as possible some issues can be encountered by different
+            assistive technology as the range of assistive technology is wide
+            and varied.
+          </p>
+        </div>
+        <div className={styles.contactBox}>
+          <h6>CONTACT US</h6>
+          <p>
+            If, at any time, you have specific questions or concerns that you
+            would like to raise with our team, please contact us at
+            contact@lorempisum.com, , +44 123 456 789 12.
+          </p>
+        </div>
+        <div className={styles.copyrightBox}>
+          <p>&copy; Copyright {currYear}</p>
+        </div>
       </div>
     </footer>
   );
