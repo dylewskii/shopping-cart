f340d41ab1143d753037e1b6e60fdb622decabdc
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-20 16:22:37 +0000

    import ProductCard & CarouselBanner

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
-import ShopBanner from "./components/shop-banner/ShopBanner";
 import HeroTwo from "./components/hero-two/HeroTwo";
-import DiscoverBanner from "./components/discover-banner/DiscoverBanner";
+import CarouselBanner from "./components/carousel-banner/CarouselBanner";
+import ProductCard from "./components/product-card/ProductCard";
 import Footer from "./components/footer/Footer";
 import SignUp from "./components/sign-up/SignUp";
 
@@ -12,9 +12,63 @@ function App() {
     <>
       <Header />
       <Hero />
-      <ShopBanner />
+      {/* ------ Shop Banner ------ */}
+      <CarouselBanner>
+        <CarouselBanner.Header>Shop Now</CarouselBanner.Header>
+        {/* ------ Banner Items ------ */}
+        <CarouselBanner.Items>
+          {/* ------ Card One ------ */}
+          <ProductCard>
+            <ProductCard.ProductImage />
+            <ProductCard.Title>Shop Product 1</ProductCard.Title>
+            <ProductCard.Description>Desc 1</ProductCard.Description>
+            <ProductCard.Price>£100.00</ProductCard.Price>
+          </ProductCard>
+          {/* ------ Card Two ------ */}
+          <ProductCard>
+            <ProductCard.ProductImage />
+            <ProductCard.Title>Shop Product 2</ProductCard.Title>
+            <ProductCard.Description>Shop Desc 2</ProductCard.Description>
+            <ProductCard.Price>£100.00</ProductCard.Price>
+          </ProductCard>
+          {/* ------ Card Three ------ */}
+          <ProductCard>
+            <ProductCard.ProductImage />
+            <ProductCard.Title>Shop Product 2</ProductCard.Title>
+            <ProductCard.Description>Shop Desc 2</ProductCard.Description>
+            <ProductCard.Price>£100.00</ProductCard.Price>
+          </ProductCard>
+        </CarouselBanner.Items>
+      </CarouselBanner>
       <HeroTwo />
-      <DiscoverBanner />
+      {/* ------ Discover Banner ------ */}
+      <CarouselBanner>
+        <CarouselBanner.Header>Discover</CarouselBanner.Header>
+        {/* ------ Banner Items ------ */}
+        <CarouselBanner.Items>
+          {/* ------ Card One ------ */}
+          <ProductCard>
+            <ProductCard.ProductImage />
+            <ProductCard.Title>Discover Product 1</ProductCard.Title>
+            <ProductCard.Description>Discover Desc 1</ProductCard.Description>
+            <ProductCard.Price>£100.00</ProductCard.Price>
+          </ProductCard>
+          {/* ------ Card Two ------ */}
+          <ProductCard>
+            <ProductCard.ProductImage />
+            <ProductCard.Title>Discover Product 2</ProductCard.Title>
+            <ProductCard.Description>Discover Desc 2</ProductCard.Description>
+            <ProductCard.Price>£100.00</ProductCard.Price>
+          </ProductCard>
+          {/* ------ Card Three ------ */}
+          <ProductCard>
+            <ProductCard.ProductImage />
+            <ProductCard.Title>Discover Product 3</ProductCard.Title>
+            <ProductCard.Description>Discover Desc 3</ProductCard.Description>
+            <ProductCard.Price>£100.00</ProductCard.Price>
+          </ProductCard>
+        </CarouselBanner.Items>
+      </CarouselBanner>
       <SignUp />
       <Footer />
     </>
--- src/components/discover-banner/DiscoverBanner.jsx
+++
@@ -1,7 +0,0 @@
-// import styles from "./DiscoverBanner.module.css";
-
-function DiscoverBanner() {
-  return;
-}
-
-export default DiscoverBanner;
