f23ec8d5fac8e08677f73281e27cb25261caddee
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-22 15:25:15 +0000

    import images for Discover section

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,11 @@ import blackAF1 from "./assets/images/shoes/air-force-1-black-1.webp";
 import whiteAF1 from "./assets/images/shoes/air-force-1-white-1.webp";
 import dunkLow from "./assets/images/shoes/dunk-low-green-1.webp";
 import v2k from "./assets/images/shoes/v2k-white-1.webp";
+import upsideDown from "./assets/images/homepage/upside-down-shoes.webp";
+import jordansInAir from "./assets/images/homepage/orange-jordans-in-air.webp";
+import furCoat from "./assets/images/homepage/fur-coat.webp";
+import sideBag from "./assets/images/homepage/side-bag.webp";
+import womanWalking from "./assets/images/homepage/woman-walking.webp";
 
 function App() {
   return (
@@ -71,24 +76,56 @@ function App() {
         <CarouselBanner.Items>
           {/* ------ Card One ------ */}
           <ProductCard>
-            <ProductCard.ProductImage />
-            <ProductCard.Title>Discover Product 1</ProductCard.Title>
-            <ProductCard.Description>Discover Desc 1</ProductCard.Description>
-            <ProductCard.Price>£100.00</ProductCard.Price>
+            <ProductCard.ProductImage
+              src={furCoat}
+              alt={"Man with a fur coat"}
+            />
+            <ProductCard.Title>Fur Coats</ProductCard.Title>
+            <ProductCard.Description>
+              From the Finest Materials.
+            </ProductCard.Description>
           </ProductCard>
           {/* ------ Card Two ------ */}
           <ProductCard>
-            <ProductCard.ProductImage />
-            <ProductCard.Title>Discover Product 2</ProductCard.Title>
-            <ProductCard.Description>Discover Desc 2</ProductCard.Description>
-            <ProductCard.Price>£100.00</ProductCard.Price>
+            <ProductCard.ProductImage
+              src={jordansInAir}
+              alt={"Shoes tied to an electricity line"}
+            />
+            <ProductCard.Title>Limited Sneakers</ProductCard.Title>
+            <ProductCard.Description>Only the Rarest.</ProductCard.Description>
           </ProductCard>
           {/* ------ Card Three ------ */}
           <ProductCard>
-            <ProductCard.ProductImage />
-            <ProductCard.Title>Discover Product 3</ProductCard.Title>
-            <ProductCard.Description>Discover Desc 3</ProductCard.Description>
-            <ProductCard.Price>£100.00</ProductCard.Price>
+            <ProductCard.ProductImage
+              src={upsideDown}
+              alt={"A person wearing shoes with their legs up"}
+            />
+            <ProductCard.Title>Walking Shoes</ProductCard.Title>
+            <ProductCard.Description>
+              Made for Everyone.
+            </ProductCard.Description>
+          </ProductCard>
+          {/* ------ Card Four ------ */}
+          <ProductCard>
+            <ProductCard.ProductImage
+              src={sideBag}
+              alt={"A man wearing a side bag"}
+            />
+            <ProductCard.Title>BAGS</ProductCard.Title>
+            <ProductCard.Description>
+              Versatile for All Occassions.
+            </ProductCard.Description>
+          </ProductCard>
+          {/* ------ Card Five ------ */}
+          <ProductCard>
+            <ProductCard.ProductImage
+              src={womanWalking}
+              alt={"A woman with sunglasses walking with her head to the side"}
+            />
+            <ProductCard.Title>Accessories</ProductCard.Title>
+            <ProductCard.Description>
+              Block Out the Rays.
+            </ProductCard.Description>
           </ProductCard>
         </CarouselBanner.Items>
       </CarouselBanner>
