84c1cc2ddc5a0b44cc760365fb60a8490d2b6f66
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-23 13:56:08 +0000

    dynamically display current year

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,6 +2,9 @@ import styles from "./Hero.module.css";
 import heroImg from "../../assets/images/homepage/rocky-twins-sat.webp";
 
 function Hero() {
+  const currYear = new Date().getFullYear();
+  const currYearShort = new Date().getFullYear().toString().slice(2);
+
   return (
     <article>
       <div className={styles.imgSection}>
@@ -14,13 +17,13 @@ function Hero() {
       </div>
       <section className={styles.contentSection}>
         <div className={styles.contentWrapper}>
-          <h2>Introducing the new SS24 COLLECTION</h2>
+          <h2>Introducing the new SS{currYearShort} COLLECTION</h2>
           <p>
-            The Spring/Summer 2024 collection presents a variety of seasonal
-            pieces that incorporate premium materials sourced from Portugal and
-            Japan; intricate wool knits, garment-dyed finishes, and more.
-            Designed with versatility in mind, each item caters to everyday
-            aesthetics. Available now.
+            The Spring/Summer {currYear} collection presents a variety of
+            seasonal pieces that incorporate premium materials sourced from
+            Portugal and Japan; intricate wool knits, garment-dyed finishes, and
+            more. Designed with versatility in mind, each item caters to
+            everyday aesthetics. Available now.
           </p>
         </div>
       </section>
