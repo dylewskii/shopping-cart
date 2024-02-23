b86f0993c401453685010e586da1568a8809be33
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-23 14:22:46 +0000

    move Homepage to pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,19 @@
+// context
+// import PageContext from "./context/PageContext";
 // css
 import "./App.css";
+// pages
+import Homepage from "./pages/Homepage";
 // components
 import Header from "./components/header/Header";
-import Hero from "./components/hero/Hero";
-import HeroTwo from "./components/hero-two/HeroTwo";
-import CarouselBanner from "./components/carousel-banner/CarouselBanner";
-import ProductCard from "./components/product-card/ProductCard";
 import Footer from "./components/footer/Footer";
 import SignUp from "./components/sign-up/SignUp";
-// context
-import DataContext from "./context/PageContext";
-// image data
-import shopImgData from "./data/shopImgData.json";
-import discoverImgData from "./data/discoverImgData.json";
 
 function App() {
   return (
     <>
       <Header />
-      <Hero />
-      {/* ------ Shop Banner ------ */}
-      <CarouselBanner>
-        <CarouselBanner.Header>Shop Now</CarouselBanner.Header>
-        <CarouselBanner.Items>
-          {shopImgData.map((shopItem) => (
-            <ProductCard key={shopItem.id}>
-              <ProductCard.ProductImage
-                src={shopItem.src.left}
-                alt={shopItem.alt}
-              />
-              <ProductCard.Title>{shopItem.name}</ProductCard.Title>
-              <ProductCard.Price>{shopItem.price}</ProductCard.Price>
-            </ProductCard>
-          ))}
-        </CarouselBanner.Items>
-      </CarouselBanner>
-      <HeroTwo />
-      {/* ------ Discover Banner ------ */}
-      <CarouselBanner>
-        <CarouselBanner.Header>Discover</CarouselBanner.Header>
-        <CarouselBanner.Items>
-          {discoverImgData.map((discoverItem) => (
-            <ProductCard key={discoverItem.id}>
-              <ProductCard.ProductImage
-                src={discoverItem.src}
-                alt={discoverItem.alt}
-              />
-              <ProductCard.Title>{discoverItem.name}</ProductCard.Title>
-              <ProductCard.Description>
-                {discoverItem.description}
-              </ProductCard.Description>
-            </ProductCard>
-          ))}
-        </CarouselBanner.Items>
-      </CarouselBanner>
+      <Homepage />
       <SignUp />
       <Footer />
     </>
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,51 @@
+// components
+import Hero from "../components/hero/Hero";
+import CarouselBanner from "../components/carousel-banner/CarouselBanner";
+import ProductCard from "../components/product-card/ProductCard";
+import HeroTwo from "../components/hero-two/HeroTwo";
+// image data
+import shopImgData from "../data/shopImgData.json";
+import discoverImgData from "../data/discoverImgData.json";
+
+export default function Homepage() {
+  return (
+    <>
+      <Hero />
+      {/* ------ Shop Banner ------ */}
+      <CarouselBanner>
+        <CarouselBanner.Header>Shop Now</CarouselBanner.Header>
+        <CarouselBanner.Items>
+          {shopImgData.map((shopItem) => (
+            <ProductCard key={shopItem.id}>
+              <ProductCard.ProductImage
+                src={shopItem.src.left}
+                alt={shopItem.alt}
+              />
+              <ProductCard.Title>{shopItem.name}</ProductCard.Title>
+              <ProductCard.Price>{shopItem.price}</ProductCard.Price>
+            </ProductCard>
+          ))}
+        </CarouselBanner.Items>
+      </CarouselBanner>
+      <HeroTwo />
+      {/* ------ Discover Banner ------ */}
+      <CarouselBanner>
+        <CarouselBanner.Header>Discover</CarouselBanner.Header>
+        <CarouselBanner.Items>
+          {discoverImgData.map((discoverItem) => (
+            <ProductCard key={discoverItem.id}>
+              <ProductCard.ProductImage
+                src={discoverItem.src}
+                alt={discoverItem.alt}
+              />
+              <ProductCard.Title>{discoverItem.name}</ProductCard.Title>
+              <ProductCard.Description>
+                {discoverItem.description}
+              </ProductCard.Description>
+            </ProductCard>
+          ))}
+        </CarouselBanner.Items>
+      </CarouselBanner>
+    </>
+  );
+}
