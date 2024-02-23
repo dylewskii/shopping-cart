9ae337e8f1fd01a63de23cdf0d80c2799f17e97d
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-23 13:49:01 +0000

    add comments

--- src/context/PageContext.jsx
+++ src/context/PageContext.jsx
@@ -13,8 +13,7 @@ export const PageProvider = ({ children }) => {
   );
 };
 
-PageProvider.propTypes = {
-  children: PropTypes.node,
-};
+// Prop Types
+PageProvider.propTypes = { children: PropTypes.node };
 
 export default PageContext;
