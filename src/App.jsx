5a111696051d9778892767d20b2798c998bcc214
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-23 14:48:29 +0000

    import DiscoverPage

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 // pages
 import HomePage from "./pages/home-page/HomePage";
 import ShopPage from "./pages/shop-page/ShopPage";
+import DiscoverPage from "./pages/discover-page/DiscoverPage";
 // components
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
