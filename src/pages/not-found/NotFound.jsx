08a0cbd2959f70ceb05ba7e6c1b928ac7932b02e
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-24 21:58:45 +0000

    add NotFound content and styles

--- src/pages/not-found/NotFound.jsx
+++ src/pages/not-found/NotFound.jsx
@@ -1,3 +1,13 @@
+import styles from "./NotFound.module.css";
+
 export default function NotFound() {
-  return <div>Error 404 - Page Not Found.</div>;
+  return (
+    <section className={styles.errorSection}>
+      <h2 className={styles.errorTitle}>😵 Error 404 😵</h2>
+      <p className={styles.errorDescription}>
+        Uh oh, we couldn&apos;t find that page, but maybe you can find the
+        perfect piece by checking out our shop? 👀
+      </p>
+    </section>
+  );
 }
