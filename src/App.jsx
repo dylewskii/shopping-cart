ce0bc0577ce8ccba7ed1af1a513634d72a07b30f
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-24 21:00:28 +0000

    set routes using React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
-// context
-// import PageContext from "./context/PageContext";
-// css
 import "./App.css";
-// pages
-import HomePage from "./pages/home-page/HomePage";
-import ShopPage from "./pages/shop-page/ShopPage";
-import DiscoverPage from "./pages/discover-page/DiscoverPage";
-// components
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
-import SignUp from "./components/sign-up/SignUp";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <HomePage />
-      <SignUp />
-      <Footer />
-    </>
-  );
+  return;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// react
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// css
+import "./index.css";
+// components / pages
+import Layout from "./Layout.jsx";
+import ShopPage from "./pages/shop-page/ShopPage.jsx";
+import DiscoverPage from "./pages/discover-page/DiscoverPage.jsx";
+import HomePage from "./pages/home-page/HomePage.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Layout>
+        <HomePage />
+      </Layout>
+    ),
+  },
+  {
+    path: "/shop",
+    element: (
+      <Layout>
+        <ShopPage />
+      </Layout>
+    ),
+  },
+  {
+    path: "/discover",
+    element: (
+      <Layout>
+        <DiscoverPage />
+      </Layout>
+    ),
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
