a9d31fe59fbea2fd21b285c1bd060dfa652322eb
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-26 17:05:52 +0000

    add CartItem component along with styles

---
+++ src/components/cart-item/CartItem.jsx
@@ -0,0 +1,60 @@
+import PropTypes from "prop-types";
+import styles from "./CartItem.module.css";
+
+export default function CartItem({ children }) {
+  return <div className={styles.cartItemBox}>{children}</div>;
+}
+
+CartItem.ProductImage = ({ src, alt }) => {
+  return (
+    <div className={styles.imageWrapper}>
+      <img src={src} alt={alt} />
+    </div>
+  );
+};
+
+CartItem.Info = ({ children }) => {
+  return <div className={styles.cartItemInfo}>{children}</div>;
+};
+
+CartItem.Title = ({ children }) => {
+  return <p className={styles.title}>{children}</p>;
+};
+
+CartItem.Description = ({ children }) => {
+  return <p className={styles.description}>{children}</p>;
+};
+
+CartItem.Color = ({ children }) => {
+  return <p className={styles.color}>{children}</p>;
+};
+
+CartItem.Price = ({ children }) => {
+  return <p className={styles.price}>{children}</p>;
+};
+
+CartItem.Remove = ({ children }) => {
+  return <button className={styles.removeBtn}>{children}</button>;
+};
+
+// Prop Types
+CartItem.propTypes = { children: PropTypes.node };
+CartItem.ProductImage.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+};
+CartItem.Info.propTypes = { children: PropTypes.node };
+CartItem.Title.propTypes = { children: PropTypes.node };
+CartItem.Description.propTypes = { children: PropTypes.node };
+CartItem.Color.propTypes = { children: PropTypes.node };
+CartItem.Price.propTypes = { children: PropTypes.node };
+CartItem.Remove.propTypes = { children: PropTypes.node };
+
+// Display Names
+CartItem.ProductImage.displayName = "CartItem.ProductImage.displayName";
+CartItem.Info.displayName = "CartItem.Info.displayName";
+CartItem.Title.displayName = "CartItem.Title.displayName";
+CartItem.Description.displayName = "CartItem.Description.displayName";
+CartItem.Color.displayName = "CartItem.Color.displayName";
+CartItem.Price.displayName = "CartItem.Price.displayName";
+CartItem.Remove.displayName = "CartItem.Remove.displayName";
