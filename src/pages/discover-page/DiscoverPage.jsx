3ef2cac67b031931fad8edee03052652b2e47a7b
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-27 15:23:34 +0000

    import BlogEntry to display blog data

--- src/pages/discover-page/DiscoverPage.jsx
+++ src/pages/discover-page/DiscoverPage.jsx
@@ -1,5 +1,20 @@
+// components
+import BlogEntry from "../../components/blog-entry/BlogEntry";
+// data
+import blogImgData from "../../data/blogImgData.json";
+// css
 import styles from "./DiscoverPage.module.css";
 
 export default function DiscoverPage() {
-  return <section className={styles.discoverPage}> </section>;
+  return (
+    <section className={styles.discoverPage}>
+      {blogImgData.map((blogItem) => (
+        <BlogEntry key={blogItem.id}>
+          <BlogEntry.BlogImage src={blogItem.src} alt={blogItem.alt} />
+          <BlogEntry.Title>{blogItem.title}</BlogEntry.Title>
+          <BlogEntry.Description>{blogItem.description}</BlogEntry.Description>
+        </BlogEntry>
+      ))}
+    </section>
+  );
 }
