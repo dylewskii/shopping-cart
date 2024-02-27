1ea3b1846519157040ddf989f33b54da47b57ddc
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-27 16:55:31 +0000

    add imgWrapper, call to action button & style each entry

--- src/components/blog-entry/BlogEntry.jsx
+++ src/components/blog-entry/BlogEntry.jsx
@@ -6,7 +6,15 @@ export default function BlogEntry({ children }) {
 }
 
 BlogEntry.BlogImage = ({ src, alt }) => {
-  return <img src={src} alt={alt} />;
+  return (
+    <div className={styles.blogImgWrapper}>
+      <img className={styles.blogImg} src={src} alt={alt} />
+    </div>
+  );
+};
+
+BlogEntry.Content = ({ children }) => {
+  return <div className={styles.content}>{children}</div>;
 };
 
 BlogEntry.Title = ({ children }) => {
@@ -17,16 +25,24 @@ BlogEntry.Description = ({ children }) => {
   return <p className={styles.description}>{children}</p>;
 };
 
+BlogEntry.CallToAction = ({ children }) => {
+  return <a className={styles.cta}>{children}</a>;
+};
+
 // Prop Types
 BlogEntry.propTypes = { children: PropTypes.node };
 BlogEntry.BlogImage.propTypes = {
   src: PropTypes.string,
   alt: PropTypes.string,
 };
+BlogEntry.Content.propTypes = { children: PropTypes.node };
 BlogEntry.Title.propTypes = { children: PropTypes.node };
 BlogEntry.Description.propTypes = { children: PropTypes.node };
+BlogEntry.CallToAction.propTypes = { children: PropTypes.node };
 // Display Names
-BlogEntry.displayName = "BlogEntry.displayName";
-BlogEntry.BlogImage.displayName = "BlogEntry.BlogImage.displayName";
-BlogEntry.Title.displayName = "BlogEntry.Title.displayName";
-BlogEntry.Description.displayName = "BlogEntry.Description.displayName";
+BlogEntry.displayName = "BlogEntry";
+BlogEntry.BlogImage.displayName = "BlogEntry.BlogImage";
+BlogEntry.Content.displayName = "BlogEntry.Content";
+BlogEntry.Title.displayName = "BlogEntry.Title";
+BlogEntry.Description.displayName = "BlogEntry.Description";
+BlogEntry.CallToAction.displayName = "BlogEntry.CallToAction";
