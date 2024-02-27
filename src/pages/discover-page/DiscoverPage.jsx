1d126d9b53a9b27d1654e48c15c9ed63072ceb9e
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-27 19:12:25 +0000

    add a marketingSeperator div,  w/ styles

--- src/pages/discover-page/DiscoverPage.jsx
+++ src/pages/discover-page/DiscoverPage.jsx
@@ -7,14 +7,27 @@ import styles from "./DiscoverPage.module.css";
 
 export default function DiscoverPage() {
   return (
-    <section className={styles.discoverPage}>
-      {blogImgData.map((blogItem) => (
-        <BlogEntry key={blogItem.id}>
-          <BlogEntry.BlogImage src={blogItem.src} alt={blogItem.alt} />
-          <BlogEntry.Title>{blogItem.title}</BlogEntry.Title>
-          <BlogEntry.Description>{blogItem.description}</BlogEntry.Description>
-        </BlogEntry>
-      ))}
-    </section>
+    <>
+      <section className={styles.discoverPage}>
+        {blogImgData.map((blogItem) => (
+          <BlogEntry key={blogItem.id}>
+            <BlogEntry.BlogImage src={blogItem.src} alt={blogItem.alt} />
+            <BlogEntry.Content>
+              <BlogEntry.Title>{blogItem.title}</BlogEntry.Title>
+              <BlogEntry.Description>
+                {blogItem.description}
+              </BlogEntry.Description>
+              <BlogEntry.CallToAction>Read More</BlogEntry.CallToAction>
+            </BlogEntry.Content>
+          </BlogEntry>
+        ))}
+      </section>
+      <div className={styles.marketingSeperator}>
+        <p>
+          Beauty, Lifestyle, Fitness, Fashion & All Your Interior Needs. Curated
+          By Experts, Sent To You Weekly.
+        </p>
+      </div>
+    </>
   );
 }
