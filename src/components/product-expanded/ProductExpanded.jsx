63b1978dd62d7ca606659301cef15b8755af4e89
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-28 20:59:09 +0000

    style entire ProductExpanded component

--- src/components/product-expanded/ProductExpanded.jsx
+++ src/components/product-expanded/ProductExpanded.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 // react
 import PropTypes from "prop-types";
 // css
 import styles from "./ProductExpanded.module.css";
+import { useState } from "react";
 
 export default function ProductExpanded({ children }) {
   return <div>{children}</div>;
@@ -15,12 +17,50 @@ ProductExpanded.ProductImage = ({ src, alt }) => {
   );
 };
 
-ProductExpanded.Details = ({ children }) => {
-  return <div className={styles.detailsBox}>{children}</div>;
+ProductExpanded.Title = ({ children }) => {
+  return <p className={styles.title}>{children}</p>;
 };
 
-ProductExpanded.Sizing = ({ children }) => {
-  return <div className={styles.sizingBox}>{children}</div>;
+ProductExpanded.Description = ({ children }) => {
+  return <p className={styles.description}>{children}</p>;
+};
+
+ProductExpanded.Price = ({ children }) => {
+  return <p className={styles.price}>{children}</p>;
+};
+
+ProductExpanded.Sizing = () => {
+  const sizes = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+  const [selectedSize, setSelectedSize] = useState(null);
+
+  const handleSizeSelection = (size) => {
+    console.log(selectedSize);
+    setSelectedSize(size);
+  };
+
+  return (
+    <>
+      <div className={styles.sizingBoxHeader}>
+        <p>Size (UK)</p>
+        <p className={styles.sizeGuide}>Size Guide</p>
+      </div>
+      <div className={styles.sizingBox}>
+        {sizes.map((size) => (
+          <div
+            key={size}
+            className={
+              selectedSize === size
+                ? `${styles.sizeCell} ${styles.selected}`
+                : `${styles.sizeCell}`
+            }
+            onClick={() => handleSizeSelection(size)}
+          >
+            {size}
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 ProductExpanded.AddToCart = () => {
@@ -38,13 +78,17 @@ ProductExpanded.ProductImage.propTypes = {
   src: PropTypes.string,
   alt: PropTypes.string,
 };
-ProductExpanded.Details.propTypes = { children: PropTypes.node };
+ProductExpanded.Title.propTypes = { children: PropTypes.node };
+ProductExpanded.Description.propTypes = { children: PropTypes.node };
+ProductExpanded.Price.propTypes = { children: PropTypes.node };
 ProductExpanded.Sizing.propTypes = { children: PropTypes.node };
 ProductExpanded.AddToCart.propTypes = { children: PropTypes.node };
 
 // Display Names
 ProductExpanded.displayName = "ProductExpanded";
 ProductExpanded.ProductImage.displayName = "ProductExpanded.ProductImage";
-ProductExpanded.Details.displayName = "ProductExpanded.Details";
+ProductExpanded.Title.displayName = "ProductExpanded.Title";
+ProductExpanded.Description.displayName = "ProductExpanded.Description";
+ProductExpanded.Price.displayName = "ProductExpanded.Price";
 ProductExpanded.Sizing.displayName = "ProductExpanded.Sizing";
 ProductExpanded.AddToCart.displayName = "ProductExpanded.AddToCart";
