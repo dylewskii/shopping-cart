fcd667795b2fa7e8c5fef0960918701abaf2cde2
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-02-29 10:28:13 +0000

    route each product to respective productId page

--- src/pages/shop-page/ShopPage.jsx
+++ src/pages/shop-page/ShopPage.jsx
@@ -1,6 +1,10 @@
+// react
+import { Link } from "react-router-dom";
 import ShopContent from "../../components/shop-content/ShopContent";
-import shopData from "../../data/shopData.json";
 import ProductCard from "../../components/product-card/ProductCard";
+// data
+import shopData from "../../data/shopData.json";
+// utils
 import formatCurrency from "../../utils/formatCurrency";
 
 export default function ShopPage() {
@@ -11,19 +15,21 @@ export default function ShopPage() {
         <ShopContent.SideBar />
         <ShopContent.Products>
           {shopData.map((shopItem) => (
-            <ProductCard key={shopItem.id}>
-              <ProductCard.ProductImage
-                src={shopItem.src.left}
-                alt={shopItem.alt}
-              />
-              <ProductCard.Title>{shopItem.brand}</ProductCard.Title>
-              <ProductCard.Description>
-                {shopItem.description} - {shopItem.color}
-              </ProductCard.Description>
-              <ProductCard.Price>
-                {formatCurrency(shopItem.price)}
-              </ProductCard.Price>
-            </ProductCard>
+            <Link to={`/product/${shopItem.id}`} key={shopItem.id}>
+              <ProductCard key={shopItem.id}>
+                <ProductCard.ProductImage
+                  src={shopItem.src.left}
+                  alt={shopItem.alt}
+                />
+                <ProductCard.Title>{shopItem.brand}</ProductCard.Title>
+                <ProductCard.Description>
+                  {shopItem.description} - {shopItem.color}
+                </ProductCard.Description>
+                <ProductCard.Price>
+                  {formatCurrency(shopItem.price)}
+                </ProductCard.Price>
+              </ProductCard>
+            </Link>
           ))}
         </ShopContent.Products>
       </ShopContent>
