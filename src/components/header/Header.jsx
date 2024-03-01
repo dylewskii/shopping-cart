54a0f30ac95bc94c65dce23d6f5215396c9ad6eb
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-03-01 21:50:49 +0000

    add ability to search products & blogs

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,20 @@
+// react
 import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import CartContext from "../../context/CartContext";
+// css
 import styles from "./Header.module.css";
+// icons
 import { IoSearch, IoCloseSharp } from "react-icons/io5";
 import { RiShoppingBagLine } from "react-icons/ri";
+// data
+import shopData from "../../data/shopData.json";
+import blogData from "../../data/blogData.json";
 
 export default function Header() {
   const { cartItems } = useContext(CartContext);
+  const [query, setQuery] = useState("");
   const [searchOpen, setSearchOpen] = useState(false);
   const [selectedLink, setSelectedLink] = useState("");
 
@@ -16,30 +23,71 @@ export default function Header() {
     setSearchOpen(!searchOpen);
   };
 
+  // const shopSearch = () => {
+  //   return shopData
+  //     .filter((item) => item.description.toLowerCase().includes(query))
+  //     .map((item) => <div key={item.id}>{item.description}</div>);
+  // };
+
+  // const blogSearch = () => {
+  //   return blogData
+  //     .filter((blog) => blog.description.toLowerCase().includes(query))
+  //     .map((blog) => <div key={blog.id}>{blog.description}</div>);
+  // };
+
   return (
     <>
       {searchOpen ? (
-        <header className={styles.searchHeader}>
-          <div className={styles.searchBox}>
-            <IoSearch
-              className={`${styles.searchIcon} ${styles.icon}`}
-              onClick={handleSearchOpen}
-            />
+        <>
+          <header className={styles.searchHeader}>
+            <div className={styles.searchBox}>
+              <IoSearch
+                className={`${styles.searchIcon} ${styles.icon}`}
+                onClick={handleSearchOpen}
+              />
+            </div>
+            <div className={styles.searchBox}>
+              <input
+                type="text"
+                className={styles.searchInput}
+                placeholder="Try searching for products or blogs."
+                onChange={(e) => setQuery(e.target.value)}
+              ></input>
+            </div>
+            <div className={styles.closeBox}>
+              <IoCloseSharp
+                className={styles.closeIcon}
+                onClick={handleSearchOpen}
+              />
+            </div>
+          </header>
+
+          <div className={styles.productSearchResultsBox}>
+            <p className={styles.searchHeading}>Products</p>
+            <div className={styles.shopSearchResults}>
+              {shopData
+                .filter((item) =>
+                  item.description.toLowerCase().includes(query)
+                )
+                .map((item) => (
+                  <div key={item.id}>{item.description}</div>
+                ))}
+            </div>
           </div>
-          <div className={styles.searchBox}>
-            <input
-              type="text"
-              className={styles.searchInput}
-              placeholder="Try searching for products or blogs."
-            ></input>
+
+          <div className={styles.blogSearchResultsBox}>
+            <p className={styles.searchHeading}>Blogs</p>
+            <div className={styles.blogSearchResults}>
+              {blogData
+                .filter((blog) =>
+                  blog.description.toLowerCase().includes(query)
+                )
+                .map((blog) => (
+                  <div key={blog.id}>{blog.description}</div>
+                ))}
+            </div>
           </div>
-          <div className={styles.closeBox}>
-            <IoCloseSharp
-              className={styles.closeIcon}
-              onClick={handleSearchOpen}
-            />
-          </div>
-        </header>
+        </>
       ) : (
         <header className={styles.header}>
           <div className={styles.iconBox}>
